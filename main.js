b33ec11dc697e1c944d709a629ef4eb5171bc0fb
Paloma Falchi <palomapfalchi@gmail.com>
2021-12-15 11:48:35 -0300

    adicionada funcao createClient, getLocalStorage e setLocalStorage,  utilizando comandos do commit 1, criada funcao read

--- main.js
+++ main.js
@@ -14,17 +14,28 @@ const tempClient = {
     celular: "(11)99999-9988",
     cidade: "São Roque"
 }
+
+//pega o que ta em localstorage (bd), transforma em JSON e armazena em db_client
+//SE FOR NULO, SE NÃO TIVER NADA NO BD. RETORNA ARRAY VAZIO
+const getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? []
+
+//manda o novo cliente para o localstorage(db) 
+const setLocalStorage = (novoClient) => localStorage.setItem("db_client", JSON.stringify(novoClient))
+
 //CRUD - Create Read Update Delete    
 
-//CREATE
+//CRUD - CREATE
 const createClient = (client) => {
-    //pega o que ta em localstorage (bd), transforma em JSON e armazena em db_client
-    const db_client = JSON.parse(localStorage.getItem("db_client"))
+    const novoClient = getLocalStorage()
     //adiciona mais 1 client (createClient(tempClient))
-    db_client.push(client)
-    //manda o novo cliente para o localstorage(db) 
-    localStorage.setItem("db_client", JSON.stringify(db_client))
+    novoClient.push(client)
+    setLocalStorage(novoClient)
 }
+
+//CRUD - READ
+const readClient =  () => getLocalStorage();
+
+
 
 //EVENTOS
 
